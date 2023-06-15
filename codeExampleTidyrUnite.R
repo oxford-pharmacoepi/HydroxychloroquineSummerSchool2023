dca6eddef5406d2e1320edf51a1f0a379110e523
Nuria Mercade <nmercade147@gmail.com>
2023-06-15 14:24:44 +0100

    example code fixed

--- codeExampleTidyrUnite.R
+++ codeExampleTidyrUnite.R
@@ -1,4 +1,17 @@
 load("atc.RData")
+
+cdm[[hcq_new_users_table_name]] <- cdm[[hcq_new_users_table_name]] %>%
+  mutate(window = case_when(
+    .data$cohort_start_date >= !! window.before[1] & .data$cohort_start_date <= !! window.before[2] ~
+      "before",
+    .data$cohort_start_date >= !! window.hcq[1] & .data$cohort_start_date <= !! window.hcq[2] ~
+      "during",
+    .data$cohort_start_date >= !! window.after[1] & .data$cohort_start_date <= !! window.after[2] ~
+      "after",
+    .default = NA
+  )) %>%
+  filter(!is.na(window)) 
+
 result_ATC_hcq <- summariseCharacteristicsFromCodelist(
   cohort = cdm[[hcq_new_users_table_name]],
   cdm = cdm,
