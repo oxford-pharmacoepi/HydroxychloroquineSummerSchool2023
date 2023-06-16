07d399624b4801e746bd8423c8ee17f1b8272b00
Nuria Mercade <nmercade147@gmail.com>
2023-06-16 17:59:05 +0100

    Day 1 folder added

---
+++ Day_1/InstantiateCohorts.R
@@ -0,0 +1,63 @@
+## Variables ----
+# NAMES FOR COHORT TABLES
+stem_table               <- "ss"
+study_table_name         <- paste0(stem_table, "_study")
+hcq_new_users_table_name <- paste0(stem_table, "_hcq_new")
+hcq_users_table_name     <- paste0(stem_table, "_hcq")
+mtx_new_users_table_name <- paste0(stem_table, "_mtx_new")
+mtx_users_table_name     <- paste0(stem_table, "_mtx")
+
+table_names <- c(study_table_name, hcq_new_users_table_name, hcq_users_table_name,
+                 mtx_new_users_table_name, mtx_users_table_name)
+
+# STUDY DATES
+study.start <- as.Date("2019-01-01")
+covid.start <- as.Date("2020-02-01")
+hcq.end     <- as.Date("2020-06-15")
+study.end   <- as.Date("2022-06-01")
+
+
+## Instantiate JSON files----
+# Package: CDMConnector
+json_cohort_set <- readCohortSet(
+  path = here("1_InstantiateCohorts", "Cohorts")
+)
+
+# Instantiate cohorts from json
+cdm <- generateCohortSet(
+  cdm = cdm,
+  cohortSet = json_cohort_set,
+  name = study_table_name,
+  computeAttrition = TRUE,
+  overwrite = TRUE
+)
+
+
+## Instantiate hydroxychloroquine ----
+# Package: CodelistGenerator + DrugUtilisation
+hcq_concept_list <- getDrugIngredientCodes(cdm, "hydroxychloroquine")
+
+# Users
+cdm <- generateDrugUtilisationCohortSet(
+  cdm = cdm,
+  name = hcq_users_table_name,
+  conceptSetList = hcq_concept_list,
+  summariseMode = "AllEras", 
+  gapEra = 30
+)
+
+# New users
+cdm <- generateDrugUtilisationCohortSet(
+  cdm = cdm,
+  name = hcq_new_users_table_name,
+  conceptSetList = hcq_concept_list,
+  summariseMode = "FirstEra",
+  daysPriorHistory = 365,
+  gapEra = 30,
+  priorUseWashout = 365,
+  cohortDateRange = as.Date(c(study.start, study.end))
+)
+
+
+
+
